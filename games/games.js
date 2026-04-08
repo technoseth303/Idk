acb0c80a23eb52694414b81271a20e3a96d051b9
technoseth303 <sethjohnbecker@gmail.com>
2026-04-08 17:22:17 +0100

    Create games.js

---
+++ games/games.js
@@ -0,0 +1,8 @@
+function openGame(page) {
+    const overlay = document.getElementById("static-overlay");
+    overlay.style.opacity = 1;
+
+    setTimeout(() => {
+        window.location.href = page;
+    }, 500);
+}
